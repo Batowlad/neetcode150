ade99e45040b11fec452065b0a6ddc8d6c175555
Batowlad <wladyslaw.pshenychnyy@gmail.com>
2026-09-03 20:04:45 -0400

    A working solution for the problem from last commit (had to look it up)

--- sliding_window/longest_substr_w-o_repeating.py
+++ sliding_window/longest_substr_w-o_repeating.py
@@ -1,17 +1,11 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int: #type: ignore
-        substring = ""
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        substring = {}
         max_len = 0
-        start, end = 0, 1
-        for x in s:
-            print(f"start:{substring}:end")
-            if x in substring:
-                if max_len < len(substring):
-                    max_len = len(substring)
-                start = end
-                substring = ""
-            substring+=x
-            end+=1
-
-        
+        start = 0
+        for end in range(len(s)):
+            if s[end] in substring:
+                start = max(substring[s[end]]+1, start)
+            substring[s[end]] = end 
+            max_len = max(max_len, end-start+1)
         return max_len
